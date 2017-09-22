d30950e0bbf9e56582a610df84e9a42657b7d1a4
Eric Fortney <eafortney2@gmail.com>
2017-09-21 22:31:35 -0600

    CreativeProject 1, final commit

---
+++ scripts/Recommendations.js
@@ -0,0 +1,6 @@
+function recommendation_message() {
+  var text;
+  text = document.getElementById("text").value;
+
+  document.getElementById("demo").innerHTML = "We will look into putting " + text + " onto our website!";
+}
--- scripts/toggleRecipePhotos.js
+++ scripts/toggleRecipePhotos.js
@@ -1,20 +1,29 @@
 function displayNextImage() {
       x = (x === images.length - 1) ? 0 : x + 1;
-      document.getElementById("toggleRecipePhotos").src = images[x];
+      document.getElementById("mainImage").src = images[x];
+      updateActiveButton(x);
   }
 
   function displayPreviousImage() {
       x = (x <= 0) ? images.length - 1 : x - 1;
-      document.getElementById("toggleRecipePhotos").src = images[x];
+      document.getElementById("mainImage").src = images[x];
+      updateActiveButton(x);
   }
 
   function startTimer() {
       setInterval(displayNextImage, 3000);
+      updateActiveButton(0);
   }
   function goToSpecificImage(index){
-    document.getElementById("toggleRecipePhotos").src = images[index];
+    document.getElementById("mainImage").src = images[index];
+    updateActiveButton(index);
+  }
+  function updateActiveButton(index){
+    document.getElementById(index).style.background = "#365b28";
   }
 
   var images = [], x = -1;
-  images[0] = "images/SpinachRavioli.jpg";
+  images[0] = "images/Spaghetti.jpg";
   images[1] = "images/LemonSquares.jpg";
+  images[2] = "images/chocolate_chip.jpg";
+  images[3] = "images/chickenNoodle.jpg";
